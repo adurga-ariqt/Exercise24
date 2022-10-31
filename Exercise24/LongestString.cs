c4342a8e69102fd2a7b8e1838366c42d6ebd8a03
adurga-ariqt <adurga@ariqt.com>
2022-10-31 14:30:04 +0530

    Add project files.

---
+++ Exercise24/LongestString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise24
+{
+    internal class LongestString
+    {
+        public static void Main()
+        {
+            string line = "Write a C# Sharp Program to display the following pattern using the alphabet.";
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+
+            Console.WriteLine(word);
+
+        }
+    }
+}
